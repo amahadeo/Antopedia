ef7956b0d0fec35690cc7d82cd5c3b3a4c8116eb
Anthony Mahadeo <anthonymahadeo@gmail.com>
2015-11-09 20:25:56 +0000

    Added Sign In/Out functionality w/ tests

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,10 @@
 FactoryGirl.define do
   factory :user do
-    
+    username "member"
+    password "123456789"
+    password_confirmation "123456789"
+    email "member@antopedia.com"
+    confirmed_at Time.now
   end
 
 end
